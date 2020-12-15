b1ce0f06dd9f629d81ab4316e7a03966c6ec757a
zgm1992 <286496127@qq.com>
2020-12-15 16:11:57 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # study-flutter-radux-demo
 学习flutter的下的radux 设计，demo工程
+test
---
+++ flutter_app_radux_demo/android/app/src/main/kotlin/com/example/flutter_app_radux_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_app_radux_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
